5f65b5fd514fec57043c8db5d41f6a62c8b93932
borsosbe <borsosbe@gmail.com>
2023-03-02 20:17:36 +0100

    Initial Commit

---
+++ DApp-Exchange/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DApp-Exchange
+//
+//  Created by Bence Borsos on 2023. 03. 02..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DApp-Exchange/DApp_ExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DApp_ExchangeApp.swift
+//  DApp-Exchange
+//
+//  Created by Bence Borsos on 2023. 03. 02..
+//
+
+import SwiftUI
+
+@main
+struct DApp_ExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
